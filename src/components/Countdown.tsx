e37d6dd191f9a8687cd59b85073b64c7ee169ffe
Renato Xavier <rnatu91@gmail.com>
2021-02-24 14:01:23 -0300

    Finalizado: componente CompletedChallenges

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,5 @@
+import styles from "../styles/components/Countdown.module.css";
+
+export function Countdown() {
+  return <div></div>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
@@ -11,6 +14,8 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
 
         <div>
