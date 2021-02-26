d8ad655499bb9436491da38265dbb5933b991e5a
Renato Xavier <rnatu91@gmail.com>
2021-02-26 12:44:11 -0300

    Contexto: atualizado

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -8,13 +8,15 @@ interface ChallengesProviderProps {
 
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
 
   function levelUp() {
     setLevel(level + 1);
   }
 
   return (
-    <ChallengesContext.Provider value={{ level, levelUp }}>
+    <ChallengesContext.Provider value={{ level, currentExperience, challengesCompleted, levelUp }}>
       {children}
     </ChallengesContext.Provider>
   );
